3012b9276d2e559afb42a8f5be3b08c252d87b8e
Théo <theo.delas@ynov.com>
2021-03-14 23:35:33 +0100

    :sparkles: Add signin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,6 @@
 import { React, useState } from "react";
+import { useDispatch } from "react-redux";
+import { signin } from "../firebase/firebase";
 import { Link } from "react-router-dom";
 import {
   Button,
@@ -9,38 +11,71 @@ import {
   Segment,
 } from "semantic-ui-react";
 
-const SignIn = () => (
+const SignIn = () => {
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
 
-  <Grid textAlign="center" verticalAlign="middle">
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header> Se connecter</Header>
-      <Form size="large">
-        <Segment>
-          <Form.Input
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="E-mail address"
-          />
-          <Form.Input
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-          />
+  const dispatch = useDispatch();
 
-          <Button secondary size="large">
-            Sign in
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us?
-        <Button as={Link} to="/signup" content="Sign Up" />
-      </Message>
-    </Grid.Column>
-  </Grid>
-);
+
+  const handleInputChange = (evt) => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
+  }
+
+  const handleSubmit = (evt) => {
+    signin(
+      state.email,
+      state.password,
+      dispatch
+    )
+    evt.preventDefault();
+  }
+
+  return(
+    <Grid textAlign="center" verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header> Se connecter</Header>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              name="email"
+              placeholder="E-mail address"
+              value={state.email}
+              onChange={handleInputChange}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              name="password"
+              placeholder="Password"
+              type="password"
+              value={state.password}
+              onChange={handleInputChange}
+            />
+
+            <Button secondary size="large">
+              Sign in
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us?
+          <Button as={Link} to="/signup" content="Sign Up" />
+        </Message>
+      </Grid.Column>
+    </Grid>
+  )
+};
 
 export default SignIn;
